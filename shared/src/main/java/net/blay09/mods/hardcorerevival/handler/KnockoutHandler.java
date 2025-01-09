de67edd4d9e7e6532c3ac64cb4e5f7136537d8d4
Calum <hendersoncal117@gmail.com>
2025-01-09 14:20:22 +0000

    fix: death message bug

--- shared/src/main/java/net/blay09/mods/hardcorerevival/handler/KnockoutHandler.java
+++ shared/src/main/java/net/blay09/mods/hardcorerevival/handler/KnockoutHandler.java
@@ -47,7 +47,7 @@ public class KnockoutHandler {
                 return;
             }
 
-            boolean canDamageSourceKnockout = !damageSource.is(DamageTypes.FELL_OUT_OF_WORLD) && !damageSource.is(HardcoreRevivalManager.NOT_RESCUED_IN_TIME);
+            boolean canDamageSourceKnockout = !damageSource.is(DamageTypes.FELL_OUT_OF_WORLD) && !damageSource.is(HardcoreRevivalManager.NOT_RESCUED_IN_TIME) && !damageSource.is(HardcoreRevivalManager.EXECUTED);
             if (canDamageSourceKnockout && isKnockoutEnabledFor(player, damageSource) && player.getHealth() - event.getDamageAmount() <= 0f) {
                 // Reduce damage to prevent the player from dying
                 event.setDamageAmount(Math.min(event.getDamageAmount(), Math.max(0f, player.getHealth() - 1f)));
