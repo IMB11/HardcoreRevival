aec61a60f7f9b4a77bdb66d16dc45876ec87dcf1
Calum <hendersoncal117@gmail.com>
2025-01-09 14:33:27 +0000

    static bool

--- shared/src/main/java/net/blay09/mods/hardcorerevival/HardcoreRevivalManager.java
+++ shared/src/main/java/net/blay09/mods/hardcorerevival/HardcoreRevivalManager.java
@@ -30,7 +30,7 @@ public class HardcoreRevivalManager {
     public static final ResourceKey<DamageType> NOT_RESCUED_IN_TIME = ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation(HardcoreRevival.MOD_ID, "not_rescued_in_time"));
     public static final ResourceKey<DamageType> EXECUTED = ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation(HardcoreRevival.MOD_ID, "executed"));
 
-    private boolean suppressDeathMessage = false;
+    private static boolean suppressDeathMessage = false;
 
     public HardcoreRevivalData getRevivalData(Player player) {
         HardcoreRevivalData provider = Balm.getProviders().getProvider(player, HardcoreRevivalData.class);
